89ffa5e926bfaf9a77904e17f6f97a0e53bb1e1e
trisgerra <andrea.digiacomo@live.it>
2025-06-07 01:27:44 +0200

    fix linting

--- src/maths.ts
+++ src/maths.ts
@@ -12,7 +12,7 @@ export function multiply(a: number, b: number): number {
 
 export function divide(a: number, b: number): number {
   if (b === 0) {
-    throw new Error("Division by zero is not allowed.");
+    throw new Error('Division by zero is not allowed.');
   }
   return a / b;
 }
